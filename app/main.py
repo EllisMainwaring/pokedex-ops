c268ce10c24f1adb8210b54b82b05fb95e662982
EllisMain <ellismain@btinternet.com>
2026-01-25 20:59:58 +0000

    Add database-backed Pokemon endpoints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
-from .database import Base, engine
+from fastapi import FastAPI, Depends
+from .database import Base, engine, get_db
 from .models import Pokemon
+from sqlalchemy.orm import Session
 
 app = FastAPI() 
 
@@ -10,3 +11,19 @@ Base.metadata.create_all(bind=engine)
 @app.get("/health")
 def health():
     return{"status": "ok"} 
+
+@app.get("/pokemon")
+def create_pokemon(name: str, db: Session = Depends(get_db)): #give me a database connection for this request
+    pokemon = Pokemon(name=name) #creates python object
+    db.add(pokemon)
+    db.commit()
+    db.refresh(pokemon)
+    return {
+        "id": pokemon.id,
+        "name": pokemon.name
+    }
+
+@app.get("/pokemon")
+def get_pokemon(db: Session = Depends(get_db)):
+    pokemon = db.query(Pokemon).all()
+    return pokemon
